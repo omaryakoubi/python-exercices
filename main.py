0d6c2c2d40036d2f80b0635cdd5d39baca0e85f5
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-17 19:42:39 +0100

    #3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Question 1 :
+
 # Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 # between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
@@ -13,6 +14,7 @@ def numbers_divisible_by_7() :
 
 
 # Question 2 :
+
 # Write a program which can compute the factorial of a given numbers.
 # The results should be printed in a comma-separated sequence on a single line.
 # Suppose the following input is supplied to the program:
@@ -28,4 +30,24 @@ def factorial(number) :
         return 1
     else :
         return number * factorial(number - 1)
+
+
+# Question 3
+
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+# Hints:
+# # In case of input data being supplied to the question, it should be assumed to be a console input.
+# Consider use dict()
+
+def generate_dictionary(number):
+    range_of_numbers = range(1, number + 1)
+    obj = {}
+    for number in range_of_numbers:
+       obj[int(number)] =  int(number * number)
+       print(obj) 
 
