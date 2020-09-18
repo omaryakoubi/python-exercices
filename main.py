0253bf3010b8452cf771e47fe41408d707eb7170
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-18 17:00:36 +0100

    #6 !WORKING

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def generate_dictionary(number):
 
 
 # Question 4 :
+
 # Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
 # Suppose the following input is supplied to the program:
 # 34,67,55,33,12,98
@@ -77,6 +78,7 @@ def sequence_to_list_and_tuple(*sequence_of_numbers):
 
 
 # Question 5 :
+
 # Define a class which has at least two methods:
 # getString: to get a string from console input
 # printString: to print the string in upper case.
@@ -95,6 +97,34 @@ class User_input_class:
     def print_string(self):
         print (self.user_input.upper())
 
-test = User_input_class()
-test.get_string()
-test.print_string() 
+# test = User_input_class()
+# test.get_string()
+# test.print_string() 
+
+
+# Question 6 :
+
+# Write a program that calculates and prints the value according to the given formula:
+# Q = Square root of [(2 * C * D)/H]
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+# D is the variable whose values should be input to your program in a comma-separated sequence.
+# Example
+# Let us assume the following comma separated input sequence is given to the program:
+# 100,150,180
+# The output of the program should be:
+# 18,22,24
+# 
+# Hints:
+# If the output received is in decimal form, it should be rounded off to its nearest value (for example, if the output received is 26.0, it should be printed as 26)
+# In case of input data being supplied to the question, it should be assumed to be a console input. 
+
+#NOT WORKING 
+
+def test(*D):
+    C = 50
+    H = 30
+    return 2 * C * D / H
+
+print(test(100,150,180))
+
